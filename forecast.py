879074ebe04ecb48ffe5c10e14abc143523f0c03
Mohammed Abdul Ghani <mabdulghani.m@gmail.com>
2018-07-05 12:46:46 -0500

    chaning kelvin to farh

--- forecast.py
+++ forecast.py
@@ -41,6 +41,8 @@ if x["cod"] != "404":
     # store the value corresponding
     # to the "temp" key of y
     current_temperature = y["temp"]
+	abc = 459.67
+	current_temperature = current_temperature - abc
  
     # store the value corresponding
     # to the "pressure" key of y
